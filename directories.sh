b23ffe65c5463116d755672be844853cb5f1c3fb
JonMS95 <jon_portugalete@hotmail.com>
2023-09-27 00:16:24 +0200

    Added input parameters so that xml source file as well as the node which contain the target directory structure can be arbitrarily selected.

--- directories.sh
+++ directories.sh
@@ -1,15 +1,68 @@
 #!/bin/bash
 
-DIR_KEYWORD="Directories/"
-PATH_CONFIG="config.xml"
 PATH_DIR_LIST="Temp/directory_list.txt"
-DIR_PREFIX="config/Directories/"
+DIRS_NODE="config/Directories/"
+
+MSG_USAGE="Usage: $0 [-c] [-h]"
+MSG_c="Location of the xml file which contains the directory structure of the project."
+MSG_d="Node of the xml file that describes the target directory structure."
+
+OPTS_SHORT="c:h"
+OPTS_LONG="config_file:,help"
+
+OPTS=$(getopt --options $OPTS_SHORT --longoptions $OPTS_LONG -- "$@")
+
+eval set -- "$OPTS"
+
+while :
+do
+    case "$1" in
+        -c | --config_file)
+            PATH_CONFIG="$2"
+            shift 2
+            ;;
+
+        -d | --directories_node)
+            DIRS_NODE="$2"
+            shift 2
+            ;;
+
+        -h | --help)
+            echo "Usage: $0 [-c arg]"
+            echo -e "-c --path_config\t${MSG_c}"
+            echo -e "-d --dirs_node\t\t${MSG_d}"
+            exit 0
+            ;;
+
+        --) shift; 
+            break 
+            ;;
+        
+        *)
+            echo "Unexpected option: $1"
+            exit 1
+            ;;
+    esac
+done
+
+list=("PATH_DIR_LIST" "DIRS_NODE")
+
+for var in "${list[@]}"
+do
+    if [ -z "${!var}" ]
+    then
+        echo "$var is NULL"
+        exit 1
+    else
+        echo "$var = ${!var}"
+    fi
+done
 
 if [ ! -d Temp ]; then
     mkdir Temp
 fi
 
-xmlstarlet el $PATH_CONFIG | grep $DIR_KEYWORD >> $PATH_DIR_LIST
+xmlstarlet el $PATH_CONFIG | grep $DIRS_NODE >> $PATH_DIR_LIST
 
 echo "********************"
 echo "Creating directories"
@@ -17,7 +70,7 @@ echo "********************"
 
 while read -r line
 do
-    new_dir=${line/#$DIR_PREFIX}
+    new_dir=${line/#$DIRS_NODE}
     if [ ! -d $new_dir ]
     then
         echo "Creating $new_dir directory ..."
