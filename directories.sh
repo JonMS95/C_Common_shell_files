fa2126fa5ad2799e0e51d5a44d32780a750bd776
JonMS95 <jon_portugalete@hotmail.com>
2023-09-29 00:31:38 +0200

    Input parameter variables were replaced by a key-value map. Same will be done in a near future for sym_links.sh. WIP

--- directories.sh
+++ directories.sh
@@ -7,15 +7,23 @@ MSG_d="Node of the xml file that describes the target directory structure."
 MSG_USAGE="Usage: $0 [-c] [-h]\r\n\
 \t-c --c --config_file\t${MSG_c}\r\n\
 \t-d --dirs_node\t\t${MSG_d}"
+MSG_NO_OPT="No input parameters were provided for $0"
 
 OPTS_SHORT="c:d:h"
 OPTS_LONG="config_file:,dirs_node:,help"
 
+# Check if no input parameter were passed.
 if [ $# -eq 0 ]; then
-    echo -e ${MSG_USAGE}
-    exit 1
+    echo  ${MSG_NO_OPT}
 fi
 
+# declare a map in which every value will store each input parameter option provided value.
+declare -A OPT_VALUES
+
+OPT_VALUES["CONFIG_FILE"]="config.xml"
+OPT_VALUES["DIRS_NODE"]="config/Directories/"
+
+# Parse options.
 OPTS=$(getopt --options $OPTS_SHORT --longoptions $OPTS_LONG -- "$@")
 
 eval set -- "$OPTS"
@@ -24,12 +32,12 @@ while :
 do
     case "$1" in
         -c | --config_file)
-            CONFIG_FILE="$2"
+            OPT_VALUES["CONFIG_FILE"]="$2"
             shift 2
             ;;
 
         -d | --directories_node)
-            DIRS_NODE="$2"
+            OPT_VALUES["DIRS_NODE"]="$2"
             shift 2
             ;;
 
@@ -49,24 +57,24 @@ do
     esac
 done
 
-list=("CONFIG_FILE" "DIRS_NODE")
-
-for var in "${list[@]}"
+# Check whether or not every option has an associated value.
+for key in "${!OPT_VALUES[@]}"
 do
-    if [ -z "${!var}" ]
+    if [ -z "${OPT_VALUES[$key]}" ];
     then
-        echo "$var is NULL"
+        echo "${key} has no associated value"    
         exit 1
     else
-        echo "$var = ${!var}"
+        echo "${key} = ${OPT_VALUES[$key]}"
     fi
 done
 
+# Generate directories.
 if [ ! -d Temp ]; then
     mkdir Temp
 fi
 
-xmlstarlet el $CONFIG_FILE | grep $DIRS_NODE >> $PATH_DIR_LIST
+xmlstarlet el ${OPT_VALUES["CONFIG_FILE"]} | grep ${OPT_VALUES["DIRS_NODE"]} >> $PATH_DIR_LIST
 
 echo "********************"
 echo "Creating directories"
@@ -74,7 +82,7 @@ echo "********************"
 
 while read -r line
 do
-    new_dir=${line/#$DIRS_NODE}
+    new_dir=${line/#${OPT_VALUES["DIRS_NODE"]}}
     if [ ! -d $new_dir ]
     then
         echo "Creating $new_dir directory ..."
