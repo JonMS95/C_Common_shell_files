3a02f325f1d4d91620b5a992f8c430efe4728a00
JonMS95 <jon_portugalete@hotmail.com>
2023-09-29 16:21:28 +0200

    Refactored file. Much prettier now. Split the code in different functions.

--- directories.sh
+++ directories.sh
@@ -1,97 +1,156 @@
 #!/bin/bash
 
-PATH_DIR_LIST="Temp/directory_list.txt"
+#######################################################################################
+# ParseOptions variables
+OPTS_SHORT="c:d:h"
+OPTS_LONG="config_file:,dirs_node:,help"
 
+# ParseOptions messages.
 MSG_c="Location of the xml file which contains the directory structure of the project."
 MSG_d="Node of the xml file that describes the target directory structure."
 MSG_USAGE="Usage: $0 [-c] [-h]\r\n\
 \t-c --c --config_file\t${MSG_c}\r\n\
 \t-d --dirs_node\t\t${MSG_d}"
+MSG_UNEXPECTED_OPT="Unexpected option has been provided: $1"
+#######################################################################################
+
+########################################################################################
+# GenerateDirectories variables
+PATH_DIR_LIST="Temp/directory_list.txt"
+
+# GenerateDirectories messages
+MSG_CREATING_DIRS="********************\r\nCreating directories\r\n********************"
+########################################################################################
+
+#####################################################
+# Main messages.
 MSG_NO_OPT="No input parameters were provided for $0"
+#####################################################
 
-OPTS_SHORT="c:d:h"
-OPTS_LONG="config_file:,dirs_node:,help"
-
-# Check if no input parameter were passed.
-if [ $# -eq 0 ]; then
-    echo  ${MSG_NO_OPT}
-fi
-
-# declare a map in which every value will store each input parameter option provided value.
+#################################################################################
+# Global variables
+# Map in which every value will store each input parameter option provided value.
 declare -A OPT_VALUES
 
+# Associate each option internal variable with its default value (if any).
 OPT_VALUES["CONFIG_FILE"]="config.xml"
 OPT_VALUES["DIRS_NODE"]="config/Directories/"
+#################################################################################
 
-# Parse options.
-OPTS=$(getopt --options $OPTS_SHORT --longoptions $OPTS_LONG -- "$@")
+#########################################################################
+# Parse input arguments passed to the current file.
+# Returns: 1 if an unexpected option or -h (help) was found, 0 otherwise.
+#########################################################################
+ParseOptions()
+{
+    # Retrieve provided option values.
+    OPTS=$(getopt --options $OPTS_SHORT --longoptions $OPTS_LONG -- "$@")
 
-eval set -- "$OPTS"
+    eval set -- "$OPTS"
 
-while :
-do
-    case "$1" in
-        -c | --config_file)
-            OPT_VALUES["CONFIG_FILE"]="$2"
-            shift 2
-            ;;
+    while :
+    do
+        case "$1" in
+            -c | --config_file)
+                OPT_VALUES["CONFIG_FILE"]="$2"
+                shift 2
+                ;;
 
-        -d | --directories_node)
-            OPT_VALUES["DIRS_NODE"]="$2"
-            shift 2
-            ;;
+            -d | --directories_node)
+                OPT_VALUES["DIRS_NODE"]="$2"
+                shift 2
+                ;;
 
-        -h | --help)
-            echo -e ${MSG_USAGE}
-            exit 0
-            ;;
+            -h | --help)
+                echo -e ${MSG_USAGE}
+                exit 1
+                ;;
 
-        --) shift; 
-            break 
-            ;;
-        
-        *)
-            echo "Unexpected option: $1"
-            exit 1
-            ;;
-    esac
-done
+            --) shift; 
+                break 
+                ;;
+            
+            *)
+                echo "$MSG_UNEXPECTED_OPT"
+                exit 1
+                ;;
+        esac
+    done
+}
 
+################################################################################
 # Check whether or not every option has an associated value.
-for key in "${!OPT_VALUES[@]}"
-do
-    if [ -z "${OPT_VALUES[$key]}" ];
+# $1: OPT_VALUES
+# Returns: 1 if any of the input arguments has no associated value, 0 otherwise.
+################################################################################
+CheckOptionValues()
+{
+    local -n options_map="$1"
+    for key in "${!options_map[@]}"
+    do
+        if [ -z "${options_map[$key]}" ];
+        then
+            echo "${key} has no associated value"
+            exit 1
+        else
+            echo "${key} = ${options_map[$key]}"
+        fi
+    done
+}
+
+################################################################################################
+# Brief: generate directories based on what is found within the nodes of the configuration file.
+# $1: OPT_VALUES["CONFIG_FILE"]
+# $2: OPT_VALUES["DIRS_NODE"]
+# $3: PATH_DIR_LIST
+# Returns: 1 if the configuration file could not be found, 0 otherwise.
+################################################################################################
+GenerateDirectories()
+{
+    local config_file="$1"
+    local dirs_node="$2"
+    local dirs_list="$3"
+
+    if [ ! -e $config_file ]
     then
-        echo "${key} has no associated value"    
+        echo "Config file $config_file could not be found."
         exit 1
-    else
-        echo "${key} = ${OPT_VALUES[$key]}"
     fi
-done
 
-# Generate directories.
-if [ ! -d Temp ]; then
-    mkdir Temp
-fi
-
-xmlstarlet el ${OPT_VALUES["CONFIG_FILE"]} | grep ${OPT_VALUES["DIRS_NODE"]} >> $PATH_DIR_LIST
-
-echo "********************"
-echo "Creating directories"
-echo "********************"
-
-while read -r line
-do
-    new_dir=${line/#${OPT_VALUES["DIRS_NODE"]}}
-    if [ ! -d $new_dir ]
-    then
-        echo "Creating $new_dir directory ..."
-        mkdir $new_dir
-    else
-        echo "$new_dir directory already exists"
+    # Generate directories.
+    if [ ! -d Temp ]; then
+        mkdir Temp
     fi
-done < $PATH_DIR_LIST
 
-if [ -d Temp ]; then
-    rm -rf Temp
-fi
+    xmlstarlet el $config_file | grep $dirs_node >> $PATH_DIR_LIST
+
+    echo -e ${MSG_CREATING_DIRS}
+
+    while read -r line
+    do
+        new_dir=${line/#$dirs_node}
+        if [ ! -d $new_dir ]
+        then
+            echo "Creating $new_dir directory ..."
+            mkdir $new_dir
+        else
+            echo "$new_dir directory already exists"
+        fi
+    done < $PATH_DIR_LIST
+
+    if [ -d Temp ]; then
+        rm -rf Temp
+    fi
+}
+
+##########################################################################################
+# Main
+##########################################################################################
+if [ $# -eq 0 ]; then echo  ${MSG_NO_OPT}; fi
+ParseOptions
+if [ $? -eq 1 ];then exit 1; fi
+CheckOptionValues OPT_VALUES
+if [ $? -eq 1 ];then exit 1; fi
+GenerateDirectories ${OPT_VALUES["CONFIG_FILE"]} ${OPT_VALUES["DIRS_NODE"]} $PATH_DIR_LIST
+if [ $? -eq 1 ];then exit 1; fi
+##########################################################################################
