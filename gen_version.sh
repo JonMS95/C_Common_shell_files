9841e3ceb01fdd1ab44b0692fdfe4b3dcee68873
JonMS95 <jon_portugalete@hotmail.com>
2023-10-06 19:15:17 +0200

    Options changed. No option based on an attribute path is going to be provided from now on. If a variable has no value (not default nor provided value), then only that variable is featured within the error message, instead of showing each variable's value. Shortened xmlstarlet sentences, so no first grep is used to parse the attribute. SO as well as header files are copied to version API directory.

--- gen_version.sh
+++ gen_version.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-################################################################################################################################################
+#################################################################################################################################################
 # ParseOptions variables
-OPTS_SHORT="c:v:m:a:h"
-OPTS_LONG="config_file:,version_attr_path:,mode_attr_path:,api_dir_name:,help"
+OPTS_SHORT="c:n:a:k:s:h"
+OPTS_LONG="config_file:,prj_info_node:,api_dir_name:,api_header_src:,api_so_src:,help"
 
 # ParseOptions variables
 MSG_c="Location of the xml file which contains the dependency structure of the project."
-MSG_v="Location of the attribute in the configuration file where version name is found."
-MSG_m="Location of the attribute in the configuration file where version mode is found."
-MSG_a="Location of the node in the configuration file where the paths to source SO files are stored."
-MSG_USAGE="Usage: $0 [-c arg] [-v arg] [-m arg] [-a arg]\r\n\
+MSG_n="Location of the node in which project information is found."
+MSG_a="Name of the directory in which new API files are meant to be copied."
+MSG_k="Location of the API header file within the project."
+MSG_s="Location of the API SO file within the project."
+MSG_USAGE="Usage: $0 [-c arg] [-n arg] [-a arg] [-k arg] [-s arg]\r\n\
 \t-c --config_file\t${MSG_c}\r\n\
-\t-v --version_attr_path\t${MSG_v}\r\n\
-\t-m --mode_attr_path\t${MSG_m}\r\n\
-\t-a --api_dir_name\t${MSG_a}\r\n\r\n\
-Example: $0 -c config.xml -v \"config/@version\" -m \"config/@version_mode\" - a API"
+\t-n --prj_info_node\t${MSG_v}\r\n\
+\t-a --api_dir_name\t${MSG_a}\r\n\
+\t-k --api_header_src\t${MSG_k}\r\n\
+\t-s --api_so_src\t\t${MSG_s}\r\n\r\n\
+Example: $0 -c \"config.xml\" -n \"config/Project_data/\" -a API -k Source_files/my_lib_api.h -s Dynamic_libraries/lib_prj.so"
 MSG_OPT_ERROR="An error ocurred while parsing option: $1"
-################################################################################################################################################
+#################################################################################################################################################
 
 #####################################################################################
 # GenAPIVersion messages
@@ -37,9 +39,10 @@ declare -A OPT_VALUES
 
 # Associate each option internal variable with its default value (if any).
 OPT_VALUES["CONFIG_FILE"]="config.xml"
-OPT_VALUES["VERSION_ATTR_PATH"]="config/@version"
-OPT_VALUES["MODE_ATTR_PATH"]="config/@version_mode"
 OPT_VALUES["API_DIR"]="API"
+OPT_VALUES["PRJ_INFO_NODE"]="config/Project_data/"
+OPT_VALUES["API_HEADER_SOURCE"]="$(find Source_files -name "*_api\.h")"
+OPT_VALUES["API_SO_SOURCE"]="$(find Dynamic_libraries -name "*\.so")"
 #################################################################################
 
 #########################################################################
@@ -67,18 +70,23 @@ ParseOptions()
                 shift 2
                 ;;
 
-            -v | --version_attr_path)
-                OPT_VALUES["VERSION_ATTR_PATH"]="$2"
-                shift 2
-                ;;
-
-            -m | --mode_attr_path)
-                OPT_VALUES["MODE_ATTR_PATH"]="$2"
+            -n | --prj_info_node)
+                OPT_VALUES["PRJ_INFO_NODE"]="$2"
                 shift 2
                 ;;
 
             -a | --api_dir_name)
                 OPT_VALUES["API_DIR"]="$2"
+                shift 2
+                ;;
+
+            -k | --api_header_src)
+                OPT_VALUES["API_HEADER_SOURCE"]="$2"
+                shift 2
+                ;;
+
+            -s | --api_so_src)
+                OPT_VALUES["API_SO_SOURCE"]="$2"
                 shift 2
                 ;;
 
@@ -107,14 +115,13 @@ ParseOptions()
 CheckOptionValues()
 {
     local -n options_map="$1"
+
     for key in "${!options_map[@]}"
     do
         if [ -z "${options_map[$key]}" ];
         then
             echo "${key} has no associated value"
             exit 1
-        else
-            echo "${key} = ${options_map[$key]}"
         fi
     done
 }
@@ -122,24 +129,30 @@ CheckOptionValues()
 ######################################################################################################
 # Brief: generate API version directory within API directory.
 # $1: OPT_VALUES["CONFIG_FILE"]
-# $2: OPT_VALUES["VERSION_ATTR_PATH"]
-# $3: OPT_VALUES["MODE_ATTR_PATH"]
-# $4: OPT_VALUES["API_DIR"]
+# $2: OPT_VALUES["API_DIR"]
+# $3: OPT_VALUES["PRJ_INFO_NODE"]
+# $3: OPT_VALUES["API_HEADER_SOURCE"]
+# $4: OPT_VALUES["API_SO_SOURCE"]
 # Returns: 1 if the version mode is not allowed or if the RELEASE version already exists, 0 otherwise.
 ######################################################################################################
 GenAPIVersion()
 {
     local config_file="$1"
-    local version_attr="$2"
-    local mode_attr="$3"
-    local API_dir="$4"
+    local API_dir="$2"
+    local prj_data_node="$3"
+    local api_header_src="$4"
+    local api_so_src="$5"
 
-    local version_node=$(xmlstarlet el -a "${config_file}" | grep -w "${version_attr}")
-    local mode_node=$(xmlstarlet el -a "${config_file}" | grep -w "${mode_attr}")
+    local prj_data_node="config/Project_data/"
 
-    local version="$(xmlstarlet sel -t -v "//$version_node" ${config_file})"
-    local mode="$(xmlstarlet sel -t -v "//$mode_node" ${config_file})"
+    local version_major=$(xmlstarlet sel -t -v "${prj_data_node}@version_major" ${config_file})
+    local version_minor=$(xmlstarlet sel -t -v "${prj_data_node}@version_minor" ${config_file})
 
+    local version="v${version_major}_${version_minor}"
+    local mode="$(xmlstarlet sel -t -v "${prj_data_node}@version_mode" ${config_file})"
+    local URL="$(xmlstarlet sel -t -v "${prj_data_node}@URL" ${config_file})"
+
+    # If version is either RELEASE or DEBUg, go on, otherwise, exit.
     if [ ${mode} != "RELEASE" ] && [ ${mode} != "DEBUG" ]
     then
         echo "${MSG_WRONG_VERSION_NAME}"
@@ -148,7 +161,11 @@ GenAPIVersion()
 
     echo -e "VERSION:\t${version}"
     echo -e "MODE:\t\t${mode}"
+    echo -e "URL:\t\t${URL}"
 
+    local new_api_dir=""
+
+    # If RELEASE version already exists, it cannot be overwritten.
     if [ ${mode} == "RELEASE" ] && [ -d "${API_dir}/${version}" ]
     then
         echo "${MSG_RELEASE_MODE_EXISTS}"
@@ -162,17 +179,27 @@ GenAPIVersion()
             version_suffix="_DEBUG"
         fi
 
-        mkdir -p ${API_dir}/${version}${version_suffix}/{Header_files,Dynamic_libraries}
+        new_api_dir="${API_dir}/${version}${version_suffix}/"
+
+        mkdir -p ${new_api_dir}{Header_files,Dynamic_libraries}
     fi
+
+    # Copy API files to newly created directories.
+    cp ${api_header_src} "${new_api_dir}Header_files"
+    cp ${api_so_src} "${new_api_dir}Dynamic_libraries"
 }
 
 #######################################################################################################################
 # Main
 #######################################################################################################################
-if [ $# -eq 0 ]; then echo  ${MSG_NO_OPT}; fi
+echo -e "*********************************************************************************************************\r\n\
+GENERATE VERSION\r\n\
+*********************************************************************************************************"
+if [ $# -eq 0 ]; then echo ${MSG_NO_OPT}; fi
 ParseOptions $@
 if [ $? -eq 1 ];then exit 1; fi
 CheckOptionValues OPT_VALUES
 if [ $? -eq 1 ];then exit 1; fi
-GenAPIVersion ${OPT_VALUES["CONFIG_FILE"]} ${OPT_VALUES["VERSION_ATTR_PATH"]} ${OPT_VALUES["MODE_ATTR_PATH"]} ${OPT_VALUES["API_DIR"]}
+GenAPIVersion ${OPT_VALUES["CONFIG_FILE"]} ${OPT_VALUES["API_DIR"]} ${OPT_VALUES["PRJ_INFO_NODE"]} ${OPT_VALUES["API_HEADER_SOURCE"]} ${OPT_VALUES["API_SO_SOURCE"]}
 if [ $? -eq 1 ];then exit 1; fi
+echo "*********************************************************************************************************"
